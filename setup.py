73d5223379c26c729f3ddf49a532afdad0fefb58
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2023-07-25 10:50:37 +0200

    Configs changed

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 import os
+import versioneer
 
 import setuptools
 from setuptools import setup
-
-with open(os.path.join('VERSION')) as version_file:
-    version = version_file.read().strip()
-
 
 def resolve_requirements(file):
     requirements = []
@@ -26,27 +23,14 @@ def read_file(file):
 
 
 setup(
-    name="Hive_ML",
-    version=version,
-    url="https://github.com/MAIA-KTH/Hive_ML.git",
-    license="GPLv3",
-    project_urls={
-        "Documentation": "https://hive-ml.readthedocs.io",
-        "Source": "https://github.com/MAIA-KTH/Hive_ML",
-        "Tracker": "https://github.com/MAIA-KTH/Hive_ML/issues",
-    },
-    author="Bendazzoli Simone",
-    author_email="simben@kth.se",
-    long_description=read_file(os.path.join(os.path.dirname(__file__), "README.md")),
-    long_description_content_type="text/markdown",
-    description="Python package to run Machine Learning Experiments, within the Hive Framework.",  # noqa: E501
+    version=versioneer.get_version(),
     packages=setuptools.find_packages(),
     package_data={
-        "": ["configs/*.yml", "configs/*.json"],
+        "": ["configs/*.yml", "configs/*.json","tutorials/*"],
     },
-    data_files=[('', ['VERSION', "requirements.txt"]), ],
+    data_files=[('', ["requirements.txt"]), ],
     # package_dir={"": "src"},
-    install_requires=resolve_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt")),
+    #install_requires=resolve_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt")),
     entry_points={
         "console_scripts": [
             "Hive_ML_extract_radiomics = Hive_ML_scripts.Hive_ML_extract_radiomics:main",
@@ -56,6 +40,7 @@ setup(
 
         ],
     },
+    cmdclass=versioneer.get_cmdclass(),
     keywords=["machine learning", "image classification", "PCR", "medical image analysis", "DCE MRI", "radiomics",
               "feature selection", "radiodynamics"],
 )
