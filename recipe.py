1d436e88926d78cb2f5c71ddb1e0bba2ce12513b
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2023-08-03 15:35:41 +0200

    HPCCM recipe.py

---
+++ recipe.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import hpccm
+
+hpccm.config.set_working_directory('/opt/code/Hive_ML')
+
+Stage0 += baseimage(image='continuumio/anaconda3:2023.03-1')
+Stage0 += gnu()
+
+Stage0 += copy(src=[
+    'requirements.txt',
+    'MLproject',
+    'setup.cfg',
+    'versioneer.py',
+    '.gitattributes',
+    'setup.py',
+    'main.py',
+    'MANIFEST.in',
+    'README.md'
+], dest='/opt/code/Hive_ML', _mkdir=True)
+Stage0 += copy(src=[
+    './docs/source/apidocs/configs/Hive_ML_config_template.json'],
+    dest='/opt/code/Hive_ML/docs/source/apidocs/configs/Hive_ML_config_template.json', _mkdir=True)
+Stage0 += copy(src=[
+    './Hive_ML/'], dest='/opt/code/Hive_ML/Hive_ML/')
+Stage0 += copy(src=[
+    './Hive_ML_scripts/'], dest='/opt/code/Hive_ML/Hive_ML_scripts/')
+Stage0 += workdir(directory="/opt/code/Hive_ML")
+Stage0 += pip(ospackages=[""], packages=["/opt/code/Hive_ML"])
+Stage0 += runscript(commands=[''])
